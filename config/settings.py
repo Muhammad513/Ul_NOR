e345c83dac04f7d613b2d12fc29dab25033d16ac
MuhammadBobur <121751236+MuhammadDevAgro@users.noreply.github.com>
2023-03-20 20:48:23 +0300

    form1

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%8-tb_9#^jdck@u9_1!o*4ghb^$%lg10$2271vqnuyl95p7nij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
