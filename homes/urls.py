7e33577d90540a487cb11b92732cc235d528c1c7
MuhammadBobur <78463001+Muhammad513@users.noreply.github.com>
2023-03-28 14:38:14 +0500

    kurs valyuta

--- homes/urls.py
+++ homes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
-from .views import homes,xarajat,ishxaqi
+from .views import homes,xarajat,ishxaqi,valyuta
 
 urlpatterns = [
     path('',homes,name='home'),
     path('xarajat/',xarajat,name='xarajat'),
-    path('ishxaqi/',ishxaqi,name='ishxaqi')
+    path('ishxaqi/',ishxaqi,name='ishxaqi'),
+    path('valyuta/',valyuta,name='valyuta')
     
 ]
--- homes/views.py
+++ homes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .models import*
 from .form import XarajatlarForm,IshxaqiForm
 import requests
+import datetime
 def homes(request):
     
     return render(request,'ul_nor/dashboard.html')
@@ -34,3 +35,25 @@ def ishxaqi(request):
     
     context={"form":form,"ishxaqqi":ishxaqqi}
     return render(request,'ul_nor/ishxaqi.html',context)    
+
+def valyuta(request):
+    
+    url1=f'https://cbu.uz/ru/arkhiv-kursov-valyut/json/USD/{datetime.datetime}/ '
+    url2=f'https://cbu.uz/ru/arkhiv-kursov-valyut/json/RUB/{datetime.datetime}/ '
+    url3=f'https://cbu.uz/ru/arkhiv-kursov-valyut/json/EUR/{datetime.datetime}/ '
+    r1=(requests.get(url1)).json()
+    r2=(requests.get(url2)).json()
+    r3=(requests.get(url3)).json()
+    
+    ishxaqqi=IshxaqiXarajati.objects.all().order_by('-id')[0:10]
+    form=IshxaqiForm()
+    if request.method == "POST":
+        forms=IshxaqiForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('ishxaqi')
+    
+    context={"form":form,"ishxaqqi":ishxaqqi,"r1":r1,"r2":r3,"r3":r2}
+    return render(request,'ul_nor/valyuta.html',context)    
+
+    
